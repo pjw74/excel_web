a3d0fabb1023d9eec546dede2e9a9173755ebcb3
jjung <jjung@jjungui-MacBookPro.local>
2023-11-17 16:17:02 +0900

    MOD page test

--- page/page.py
+++ page/page.py
@@ -19,29 +19,23 @@ with col3:
     fname1 = st.text_input("참관자")
     #st.title(fname1)
 
-st.write('\n')
+
 st.write('채점은 색이 표시된 부분에 1(저) ~ 5(고)점까지 표시하면 됩니다.')
-st.write('\n')
 
-col4, col5, col6 = st.columns([1,5,1])
+col5, col6, col7 = st.columns([1,5,1])
 
-with col1:
-    # column 1 에 담을 내용
-
+with col5:
     st.write('채점항목')
     st.write('\n')
     st.write('\n')
     st.write('\n')
     st.write('\n')
-
-
     st.write('학습 내용의 적합성')
 
-with col2:
+with col6:
     # column 2 에 담을 내용
     st.write("상세 채점 기준")
     st.write('\n')
-
     test2 = st.write('◆ 학습목표 설정 및 수업 실연에서 일치 여부')
     test3 = st.write(' - 학습목표의 설정은 적합한가?')
     test4 = st.write(' - 학습목표의 의미를 적절한 방법으로 전달하였는가?')
@@ -50,8 +44,7 @@ with col2:
     test7 = st.write(' - 학습자의 수준에 맞는 과학적 용어나 예시 등을 적절하게 사용하였는가?')
 
 #st.title(fname)
-with col3:
-    # column 3 에 담을 내용
+with col7:
     st.write("점수")
     st.write('\n')
     st.write('\n')
