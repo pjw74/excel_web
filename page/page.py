0e501568291253ee85fbc90f88a0cf51e45ba13d
jjung <jjung@jjungui-MacBookPro.local>
2023-11-17 16:21:53 +0900

    MOD page test

--- page/page.py
+++ page/page.py
@@ -21,12 +21,13 @@ with col3:
 
 
 st.write('\n')
-st.write('채점은 색이 표시된 부분에 1(저) ~ 5(고)점까지 표시하면 됩니다.')
+st.write('채점은 1(저) ~ 5(고)점까지 표시하면 됩니다.')
 
-col5, col6, col7 = st.columns([1,4,1])
+col5, col6, col7 = st.columns([1,3,1])
 
 with col5:
     st.write('채점항목')
+    st.write('\n')
     st.write('\n')
     st.write('\n')
     st.write('\n')
@@ -49,8 +50,6 @@ with col6:
 #st.title(fname)
 with col7:
     st.write("점수")
-    st.write('\n')
-    st.write('\n')
     st.write('\n')
     st.write('\n')
     st.write('\n')
