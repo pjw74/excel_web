4f0165486c2018d6c7010d6daf917b5063bcdeef
jjung <jjung@jjungui-MacBookPro.local>
2023-11-17 15:56:58 +0900

    MOD page test

--- page/page.py
+++ page/page.py
@@ -3,27 +3,55 @@ import streamlit as st
 
 st.title("통합과학교재연구및 지도 수업시연 평가지")
 
-col1,col2, col3 = st.columns([1,1,1])
+col1, col2, col3 = st.columns([1,1,1])
 # 공간을 2:3 으로 분할하여 col1과 col2라는 이름을 가진 컬럼을 생성합니다.
 
 with col1:
     # column 1 에 담을 내용
     my_date = st.date_input('발표일시') # 디폴트 오늘
     #st.write(my_date)
-
 with col2:
     # column 2 에 담을 내용
-    fname = st.text_input("발표자: ")
+    fname = st.text_input("발표자")
     #st.title(fname)
 with col3:
-    # column 2 에 담을 내용
-    fname1 = st.text_input("참관자: ")
+    # column 3 에 담을 내용
+    fname1 = st.text_input("참관자")
     #st.title(fname1)
 
 st.write('채점은 색이 표시된 부분에 1(저) ~ 5(고)점까지 표시하면 됩니다.')
 
 
+col4, col5, col6 = st.columns([1,5,1])
 
+with col1:
+    # column 1 에 담을 내용
+    score = st.text_input('채점항목')
+    test1 = st.text_input('학습 내용의 적합성')
+
+    #st.write(my_date)
+with col2:
+    # column 2 에 담을 내용
+    fname = st.text_input("상세 채점 기준")
+    test2 = st.text_input('◆ 학습목표 설정 및 수업 실연에서 일치여부')
+    test3 = st.text_input(' - 학습목표의 설정은 적합한가?')
+    test4 = st.text_input(' - 학습목표의 의미를 적절한 방법으로 전달하였는가?')
+    test5 = st.text_input('◆ 학습자의 수준을 고려한 수업 ')
+    test6 = st.text_input(' - 학습동기 유발이 제대로 이루어졌는가?')
+    test7 = st.text_input(' - 학습자의 수준에 맞는 과학적 용어나 예시 등을 적절하게 사용하였는가?')
+
+#st.title(fname)
+with col3:
+    # column 3 에 담을 내용
+    fname1 = st.text_input("점수")
+    st.number_input('숫자 입력', 1, 5)
+    st.number_input('숫자 입력', 1, 5)
+    st.number_input('숫자 입력', 1, 5)
+    st.number_input('숫자 입력', 1, 5)
+    st.number_input('숫자 입력', 1, 5)
+    st.number_input('숫자 입력', 1, 5)
+
+    #st.title(fname1)
 
 st.write('\n')
 st.write('\n')
