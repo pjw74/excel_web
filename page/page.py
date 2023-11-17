5b3f38a0b7a82393e5823e2564460005dbdedb96
jjung <jjung@jjungui-MacBookPro.local>
2023-11-17 15:32:19 +0900

    MOD page test

--- page/page.py
+++ page/page.py
@@ -1,20 +1,18 @@
-import numpy as np
-import pandas as pd
-from sklearn.datasets import load_iris
-import matplotlib.pyplot as plt
 import streamlit as st
 
-iris_dataset = load_iris()
+col1,col2 = st.columns([2,3])
+# 공간을 2:3 으로 분할하여 col1과 col2라는 이름을 가진 컬럼을 생성합니다.
 
-df= pd.DataFrame(data=iris_dataset.data,columns= iris_dataset.feature_names)
-df.columns= [ col_name.split(' (cm)')[0] for col_name in df.columns] # 컬럼명을 뒤에 cm 제거하였습니다
-df['species']= iris_dataset.target
+with col1 :
+    # column 1 에 담을 내용
+    st.title('here is column1')
+with col2 :
+    # column 2 에 담을 내용
+    st.title('here is column2')
+    st.checkbox('this is checkbox1 in col2 ')
 
 
-species_dict = {0 :'setosa', 1 :'versicolor', 2 :'virginica'}
-
-def mapp_species(x):
-    return species_dict[x]
-
-df['species'] = df['species'].apply(mapp_species)
-print(df)
+# with 구문 말고 다르게 사용 가능
+col1.subheader(' i am column1  subheader !! ')
+col2.checkbox('this is checkbox2 in col2 ')
+#=>위에 with col2: 안의 내용과 같은 기능을합니다
