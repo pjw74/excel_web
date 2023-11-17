71cfee4f3e00f0aa77285aa12ca97013373918ef
jjung <jjung@jjungui-MacBookPro.local>
2023-11-17 15:50:57 +0900

    MOD page test

--- page/page.py
+++ page/page.py
@@ -1,9 +1,7 @@
 import streamlit as st
 
 
-
 st.title("통합과학교재연구및 지도 수업시연 평가지")
-
 
 col1,col2, col3 = st.columns([1,1,1])
 # 공간을 2:3 으로 분할하여 col1과 col2라는 이름을 가진 컬럼을 생성합니다.
@@ -11,37 +9,30 @@ col1,col2, col3 = st.columns([1,1,1])
 with col1:
     # column 1 에 담을 내용
     my_date = st.date_input('발표일시') # 디폴트 오늘
-    st.write(my_date)
+    #st.write(my_date)
+
 with col2:
     # column 2 에 담을 내용
     fname = st.text_input("발표자: ")
-    st.title(fname)
+    #st.title(fname)
 with col3:
     # column 2 에 담을 내용
     fname1 = st.text_input("참관자: ")
-    st.title(fname1)
+    #st.title(fname1)
 
 st.write('채점은 색이 표시된 부분에 1(저) ~ 5(고)점까지 표시하면 됩니다.')
 
-message = st.text_area("총평")
-st.write(message)
-
-st.write('합계')
 
 
-col1,col2 = st.columns([2,3])
-# 공간을 2:3 으로 분할하여 col1과 col2라는 이름을 가진 컬럼을 생성합니다.
 
-with col1 :
-    # column 1 에 담을 내용
-    st.title('here is column1')
-with col2 :
-    # column 2 에 담을 내용
-    st.title('here is column2')
-    st.checkbox('this is checkbox1 in col2 ')
+st.write('\n')
+st.write('\n')
+st.write('\n')
+st.write('\n')
+st.write('\n')
+st.write('\n')
 
+summary = st.text_area("총평")
+st.write(summary)
 
-# with 구문 말고 다르게 사용 가능
-col1.subheader(' i am column1  subheader !! ')
-col2.checkbox('this is checkbox2 in col2 ')
-#=>위에 with col2: 안의 내용과 같은 기능을합니다
+all_score = st.write('합계')
