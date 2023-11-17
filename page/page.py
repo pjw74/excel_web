ed2565b00d1c8437f7b8de7015a9ef8161cfdd5f
jjung <jjung@jjungui-MacBookPro.local>
2023-11-17 15:48:36 +0900

    MOD page test

--- page/page.py
+++ page/page.py
@@ -4,22 +4,29 @@ import streamlit as st
 
 st.title("통합과학교재연구및 지도 수업시연 평가지")
 
-# 6. 날짜 입력
-my_date = st.date_input('발표일시') # 디폴트 오늘
-st.write(my_date)
 
-fname = st.text_input("발표자: ")
-st.title(fname)
+col1,col2, col3 = st.columns([1,1,1])
+# 공간을 2:3 으로 분할하여 col1과 col2라는 이름을 가진 컬럼을 생성합니다.
 
-fname1 = st.text_input("참관자: ")
-st.title(fname1)
+with col1:
+    # column 1 에 담을 내용
+    my_date = st.date_input('발표일시') # 디폴트 오늘
+    st.write(my_date)
+with col2:
+    # column 2 에 담을 내용
+    fname = st.text_input("발표자: ")
+    st.title(fname)
+with col3:
+    # column 2 에 담을 내용
+    fname1 = st.text_input("참관자: ")
+    st.title(fname1)
 
 st.write('채점은 색이 표시된 부분에 1(저) ~ 5(고)점까지 표시하면 됩니다.')
 
-
-message = st.text_area("메시지를 입력하세요")
+message = st.text_area("총평")
 st.write(message)
 
+st.write('합계')
 
 
 col1,col2 = st.columns([2,3])
